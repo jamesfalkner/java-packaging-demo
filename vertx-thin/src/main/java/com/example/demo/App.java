0f3d9bdd535a0a2f0993118be63cb97cc8519f73
jamesfalkner <schtool@gmail.com>
2017-10-03 18:50:17 -0700

    updates for vertx-thin and naming

---
+++ vertx-thin/src/main/java/com/example/demo/App.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+public class App extends AbstractVerticle {
+
+    @Override
+    public void start(Future<Void> fut) {
+        vertx
+                .createHttpServer()
+                .requestHandler(r -> {
+                    r.response().end("Hello from Vert.x Thin JAR application");
+                })
+                .listen(8080, result -> {
+                    if (result.succeeded()) {
+                        fut.complete();
+                    } else {
+                        fut.fail(result.cause());
+                    }
+                });
+    }
+}
